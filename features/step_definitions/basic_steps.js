3b5ba5fc79eacfe69b645486c76f8aad850a4302
Laura Reale <laaru1988@gmail.com>
2019-01-29 15:55:58 +0100

    create openHomePage helper method in AddressBookWorld class, use setWorldConstructor module on AddressBookWorld class

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,7 +1,7 @@
 const { Given, Then } = require("cucumber");
 
 Given("I visit the site", async function() {
-        return await this.openHomePage()
+  return await this.openHomePage()
 });
 
 Then("I should see {string}", function(string) {
@@ -12,17 +12,17 @@ Then("I should see {string}", function(string) {
 Then('I click {string}', async function(string) {
   // Write code here that turns the phrase above into concrete actions
   return 'pending'
-})
+});
 
 Then('I fill in {string} with {string}', async function(string, string2) {
   // Write code here that turns the phrase above into concrete actions
   return 'pending'
-})
+});
 
 Then('I should have {int} contact in my address book', async function(int) {
   // Write code here that turns the phrase above into concrete actions
   return 'pending'
-})
+});
 
 Then('I should not see {string}', async function(string) {
   // Write code here that turns the phrase above into concrete actions
