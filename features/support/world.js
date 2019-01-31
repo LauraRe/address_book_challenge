7467e3633b99cfecbfa4572a2f84225375016646
Laura Reale <laaru1988@gmail.com>
2019-01-31 19:21:23 +0100

    fix delete button functionality

--- features/support/world.js
+++ features/support/world.js
@@ -1,55 +1,55 @@
 const { setWorldConstructor } = require('cucumber')
 const { expect } = require('chai')
-const  puppeteer  = require('puppeteer')
+const puppeteer = require('puppeteer')
 
 const HOME_PAGE = 'http://localhost:3000'
 
 class AddressBookWorld {
-    constructor() {}
+    constructor() { }
 
     async openHomePage() {
-        this.browser = await puppeteer.launch({headless: false, slowmo: 100})
+        this.browser = await puppeteer.launch({ headless: false, slowmo: 100 })
         this.page = await this.browser.newPage()
         await this.page.goto(HOME_PAGE)
     }
 
     async closeHomePage() {
-    await this.browser.close()
+        await this.browser.close()
     }
-    
+
     async pageHasTextContent(expectedContent) {
-    const pageContent = await this.page.content()
-    const actualContent = pageContent.match(expectedContent)[0]
-    expect(actualContent).to.be.eq(expectedContent)
+        const pageContent = await this.page.content()
+        const actualContent = pageContent.match(expectedContent)[0]
+        expect(actualContent).to.be.eq(expectedContent)
     }
 
     async clickOnAddContactBtn(btnName) {
-    const btnSelector = this.btnSelectorFromName(btnName.toLowerCase())
-    await this.page.waitForSelector(btnSelector)
-    await this.page.click(btnSelector)
+        const btnSelector = this.btnSelectorFromName(btnName.toLowerCase())
+        await this.page.waitForSelector(btnSelector)
+        await this.page.click(btnSelector)
     }
 
     async fillFormField(field, content) {
-    const inputSelector = `#contact-${field}`
-    await this.page.waitForSelector(inputSelector)
-    this.inputElement = await this.page.$(inputSelector)
-    await this.inputElement.type(content)
+        const inputSelector = `#contact-${field}`
+        await this.page.waitForSelector(inputSelector)
+        this.inputElement = await this.page.$(inputSelector)
+        await this.inputElement.type(content)
     }
-    
+
     async checkContactStorageCount(expectedCount) {
         const actualCount = await this.page.evaluate(
             () => JSON.parse(window.localStorage.getItem('contacts')).length
         )
         expect(actualCount).to.be.eq(expectedCount)
-    } 
-    
+    }
+
     async pageDoesNotHaveTextContent(unexpectedContent) {
         const pageContent = await this.page.content()
         let actualContent = pageContent.match(unexpectedContent)
-    
+
         expect(actualContent).to.be.eq(null)
     }
-    
+
     btnSelectorFromName(btnName) {
         switch (btnName) {
             case 'add contact':
@@ -57,7 +57,10 @@ class AddressBookWorld {
                 break
             case 'save contact':
                 return '.save-contact'
-                break   
+                break
+            case 'delete contact':
+                return '.delete-contact'
+                break
             default:
                 throw `${btnName} button is not defined`
                 break
--- src/app.js
+++ src/app.js
@@ -6,65 +6,58 @@ const renderContacts = () => {
     const contacts = JSON.parse(storage.getItem('contacts'))
     let div = document.querySelector('.contact-list')
 
-    if (contacts) {
+    if (contacts && contacts.length > 0) {
         div.innerHTML = ''
         const ul = document.createElement('ul')
         contacts.forEach(contact => {
             let li = document.createElement('li')
             li.innerHTML = `
             <div class="card">
-                <div class="content">
+                <div id="${contact.id}" class="content">
                     <h1>${ contact.name}</h1>
                     <h2>${ contact.company}</h2>
                     <p>${ contact.notes}</p>
-                    ${ contact.email} |
+                    ${ contact.email}
                     <button class="delete-contact">Delete Contact</button>
                     <button class="edit-contact">Edit Contact</button>
                     <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
-
-                    </div>
+                </div>
             </div>
-        `
+            `
             ul.appendChild(li)
-            let button = document.createElement('button');
-            button.classList += "delete-contact";
-            button.innerHTML = 'X';
-            li.appendChild(button)
-            
-            ul.appendChild(li)
+            let delete_button = li.querySelector('.delete-contact')
+            delete_button.addEventListener('click', event => {
+                let id = event.target.parentNode.id
+                let contacts = JSON.parse(storage.getItem('contacts')) || []
+                debugger
+                contacts.forEach(contact => {
+                    contact.id == id ? contacts.splice(contacts.indexOf(contact), 1) : false
+                })
+
+                console.log(contacts)
+
+                storage.setItem('contacts', JSON.stringify(contacts))
+                renderContacts()
+            })
+
         })
         div.appendChild(ul)
+
     } else {
         div.innerHTML = '<p>You have no contacts in your address book</p>'
     }
 }
 
-    let delete_button = document.querySelector('.contact-list')
-
-    delete_button.addEventListener('click',event => {
-        let id = event.target.parentNode.id
-        let contacts = JSON.parse(storage.getItem('contacts') || []
-        console.log(contacts)
-        contacts.forEach(contact => {
-
-            contact.id == id ? contacts.splice(contacts.indexOf(contact),1) : false
-        })
-
-        console.log(contacts)
-
-        storage.setItem('contacts', JSON.stringify(contacts))
-        renderContacts()
-        )
-    })
 
 document.addEventListener('DOMContentLoaded', () => {
     renderContacts()
+    debugger
     const addContactForm = document.querySelector('.new-contact-form')
 
     addContactForm.addEventListener('submit', event => {
         event.preventDefault()
 
-        const storage = window.localStorage    
+        const storage = window.localStorage
 
         const {
             name,
@@ -84,6 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
             notes: notes.value,
             twitter: twitter.value,
         }
+
         //console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
         let contacts = JSON.parse(storage.getItem('contacts')) || []
         contacts.push(contact)
